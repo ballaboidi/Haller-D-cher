cbb45f9229c3fc5194868abf331dc6c11fb61a22
Mario Autherith <mario.ta@gmx.at>
2017-03-17 15:33:58 +0100

    slideshow added
    added slideshow, not satisfied yet due do the space on the first two
    pictures, will get into it prob later

--- slideshow.js
+++ slideshow.js
@@ -1,23 +1,13 @@
-var images = ["../img/showcase1.jpeg", "../img/showcase2.jpg", "./img/showcase3.JPG"];
+//Array of images which you want to show: Use path you want.
+var images=new Array('./img/standard roof.jpeg','./img/Solarroof.jpg','./img/worker.JPG');
+var nextimage=0;
+doSlideshow();
 
-var imageNumber = 0;
-var imageLength = images.length - 1;
-
-function changeImage(x) {
-  imageNumber += x;
-  //If you reached end of array, start over!
-  if (imageNumber > imageLength) {
-      imageNumber = 0;
-  }
-  if (imageNumber < 0) {
-      imageNumber = imageLength;
-  }
-
-  document.getElementById("showcase").css("background").url = images[imageNumber];
-
-  return false;
-}
-
-function autoRun() {
-  setInterval("changeImage(1)", 3000);
+function doSlideshow(){
+    if(nextimage>=images.length){nextimage=0;}
+    $('#showcase')
+    .css('background','url("'+images[nextimage++]+'") no-repeat center')
+    .fadeIn(1000,function(){
+        setTimeout(doSlideshow,5000);
+    });
 }
